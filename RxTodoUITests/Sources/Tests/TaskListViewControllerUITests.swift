14b436b5eac13c197d2e538c819ca9dfe115a228
cruisediary <cruzdiary@gmail.com>
2016-12-11 19:38:59 +0900

    test(uitest): Add TaskAdd UITest

--- RxTodoUITests/Sources/Tests/TaskListViewControllerUITests.swift
+++ RxTodoUITests/Sources/Tests/TaskListViewControllerUITests.swift
@@ -14,4 +14,19 @@ class TaskListViewControllerUITests: UITestCase {
     let expectCount: UInt = 3
     XCTAssertEqual(app.tables.element.cells.count, expectCount, "it should have 3 tasks as default")
   }
+  
+  func testTaskAdd() {
+    let beforeCount: UInt = app.tables.element.cells.count
+    let expectCount = UInt(beforeCount + 1)
+    
+    XCTAssertTrue(app.navigationBars.buttons["Add"].exists)
+    XCTAssertTrue(app.navigationBars.buttons["Add"].isHittable)
+    app.navigationBars.buttons["Add"].tap()
+    
+    app.textFields.element.tap()
+    app.textFields.element.typeText("Hello")
+    app.navigationBars.buttons["Done"].tap()
+    
+    XCTAssertEqual(app.tables.element.cells.count, expectCount, "it should add a new task")
+  }
 }
