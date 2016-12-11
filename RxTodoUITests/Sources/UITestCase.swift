17abdf81fdeecf2126157929f0d3c8feeff0a03d
cruisediary <cruzdiary@gmail.com>
2016-12-11 18:01:41 +0900

    chore: Add RxTodoUITests to project

---
+++ RxTodoUITests/Sources/UITestCase.swift
@@ -0,0 +1,33 @@
+//
+//  RxTodoUITests.swift
+//  RxTodoUITests
+//
+//  Created by cruz on 11/12/2016.
+//  Copyright © 2016 Suyeol Jeon. All rights reserved.
+//
+
+import XCTest
+
+class UITestCase: XCTestCase {
+    
+    var app: XCUIApplication!
+    
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        app = XCUIApplication()
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        app.launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+}
