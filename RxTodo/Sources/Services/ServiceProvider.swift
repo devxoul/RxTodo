35b5f07a552375cf110eedcedddb5b077e17e09e
Suyeol Jeon <devxoul@gmail.com>
2017-01-12 05:41:15 +0900

    Add ServiceProvider

---
+++ RxTodo/Sources/Services/ServiceProvider.swift
@@ -0,0 +1,13 @@
+//
+//  ServiceProvider.swift
+//  RxTodo
+//
+//  Created by Suyeol Jeon on 12/01/2017.
+//  Copyright © 2017 Suyeol Jeon. All rights reserved.
+//
+
+protocol ServiceProviderType: class {
+}
+
+final class ServiceProvider: ServiceProviderType {
+}
