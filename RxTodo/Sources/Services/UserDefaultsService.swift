8c32d718bafe3b64d0315033f85c857a9a7188b6
Suyeol Jeon <devxoul@gmail.com>
2017-01-12 05:42:19 +0900

    Add UserDefaultsService

---
+++ RxTodo/Sources/Services/UserDefaultsService.swift
@@ -0,0 +1,34 @@
+//
+//  UserDefaultsService.swift
+//  RxTodo
+//
+//  Created by Suyeol Jeon on 12/01/2017.
+//  Copyright © 2017 Suyeol Jeon. All rights reserved.
+//
+
+import Foundation
+
+extension UserDefaultsKey {
+  static var tasks: Key<[[String: Any]]> { return "tasks" }
+}
+
+protocol UserDefaultsServiceType {
+  func value<T>(forKey key: UserDefaultsKey<T>) -> T?
+  func set<T>(value: T?, forKey key: UserDefaultsKey<T>)
+}
+
+final class UserDefaultsService: BaseService, UserDefaultsServiceType {
+
+  private var defaults: UserDefaults {
+    return UserDefaults.standard
+  }
+
+  func value<T>(forKey key: UserDefaultsKey<T>) -> T? {
+    return self.defaults.value(forKey: key.key) as? T
+  }
+
+  func set<T>(value: T?, forKey key: UserDefaultsKey<T>) {
+    self.defaults.set(value, forKey: key.key)
+  }
+
+}
