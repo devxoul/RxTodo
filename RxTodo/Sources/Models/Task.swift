78574e5e973e0c05d679cdb4d19c38d574916c24
Suyeol Jeon <devxoul@gmail.com>
2017-01-15 03:39:41 +0900

    Add isDone property to Task

--- RxTodo/Sources/Models/Task.swift
+++ RxTodo/Sources/Models/Task.swift
@@ -13,10 +13,34 @@ struct Task: ModelType, Identifiable {
   var id: String = UUID().uuidString
   var title: String
   var memo: String?
+  var isDone: Bool = false
 
   init(title: String, memo: String? = nil) {
     self.title = title
     self.memo = memo
+  }
+
+  init?(dictionary: [String: Any]) {
+    guard let id = dictionary["id"] as? String,
+      let title = dictionary["title"] as? String
+    else { return nil }
+
+    self.id = id
+    self.title = title
+    self.memo = dictionary["memo"] as? String
+    self.isDone = dictionary["isDone"] as? Bool ?? false
+  }
+
+  func asDictionary() -> [String: Any] {
+    var dictionary: [String: Any] = [
+      "id": self.id,
+      "title": self.title,
+      "isDone": self.isDone,
+    ]
+    if let memo = self.memo {
+      dictionary["memo"] = memo
+    }
+    return dictionary
   }
 
 }
